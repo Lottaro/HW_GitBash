1623a819efaf1e2609eeb46a446148cef49e714f
Sergey Karabekov <51506930+Lottaro@users.noreply.github.com>
2022-05-23 13:25:55 +0300

    Add files via upload

---
+++ GitBash_Script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+cd QA
+mkdir folder_qa_1 folder_qa_2 folder_qa_3
+cd folder_qa_1
+touch file_1.txt file_2.txt file_3.txt file_4.json file_5.json
+mkdir folder_qa_1_1 folder_qa_1_2 folder_qa_1_3
+ls -la
+mv file_1.txt file_2.txt folder_qa_1_1
